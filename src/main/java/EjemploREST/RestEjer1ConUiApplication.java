87f77674462044fcb7678a82f7a5b729e3075d82
jagonmes <65248503+jagonmes@users.noreply.github.com>
2023-12-16 11:47:12 +0100

    Añadido el ejemplo de REST

---
+++ src/main/java/EjemploREST/RestEjer1ConUiApplication.java
@@ -0,0 +1,12 @@
+package EjemploREST;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestEjer1ConUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestEjer1ConUiApplication.class, args);
+	}
+}
