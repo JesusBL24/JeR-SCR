de9d10d66f15a72e53708299dca27770fdf35bfd
jagonmes <65248503+jagonmes@users.noreply.github.com>
2023-12-18 20:08:22 +0100

    Actualización a la clase y el servicio REST de los Usuarios
    Se ha terminado la implementación de los métodos GET, POST, PUT y DELETE de los usuarios

--- src/main/java/Main/Clases/Usuario.java
+++ src/main/java/Main/Clases/Usuario.java
@@ -1,8 +1,7 @@
 package Main.Clases;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.io.*;
+import java.util.ArrayList;
 
 public class Usuario {
     private String nombre;
@@ -37,6 +36,7 @@ public class Usuario {
                 '}';
     }
 
+    //FUNCIÓN USADA PARA RECUPERAR UN USUARIO EXISTENTE EN LA BASE DEDATOS
     public static Usuario recuperarUsuario(String nombreDeUsuario){
         Usuario usuario = null;
 
@@ -53,30 +53,149 @@ public class Usuario {
 
                 //SI EL NOMBRE DE USUARIO COINCIDE, DEVOLVEMOS EL USUARIO
                 if(partes[0].equals(nombreDeUsuario)){
+                    //CREAMOS EL USUARIO
                     usuario = new Usuario(partes[0], partes[1]);
+                    //CERRAMOS EL ARCHIVO
+                    br.close();
+                    fr.close();
+                    //DEVOLVEMOS EL USUARIO
                     return usuario;
                 }
             }
+            //CERRAMOS EL ARCHIVO
+            br.close();
+            fr.close();
         }
         catch(Exception e){
             e.printStackTrace();
         }
 
+        //SI ALGO FALLA, DEVOLVEMOS NULL
         return usuario;
     }
 
+    //FUNCIÓN USADA PARA INSERTAR UN USUARIO EN LA BASE DE DATOS
     public static boolean insertarUsuario(Usuario usuario){
-        try (FileWriter fichero = new FileWriter("RecursosRest/Usuarios"))
-        {
+        //ABRIMOS EL ARCHIVO, Y CREAMOS LO NECESARIO PARA ESCRIBIR
+        try (FileWriter fw = new FileWriter("RecursosRest/Usuarios", true)) {
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter pw = new PrintWriter(bw);
 
+            //INSERTAMOS EL NUEVO USUARIO AL FINAL DEL ARCHIVO
+            pw.println(usuario.getNombre() +":"+usuario.getPassword());
 
-                fichero.write("\n");
+            //CERRAMOS EL ARCHIVO
+            pw.close();
+            bw.close();
+            fw.close();
 
-
-
+            //DEVOLVEMOS TRUE
+            return true;
         } catch (Exception e) {
+            //SI ALGO FALLA, DEVOLVEMOS FALSE
             e.printStackTrace();
+            return false;
         }
-        return false;
+
+    }
+
+    //FUNCIÓN USADA PARA ELIMINAR USUARIOS DE LA BASE DE DATOS
+    public static boolean eliminarUsuario(Usuario usuario){
+
+        String usuarioABorrar = usuario.getNombre() + ":" + usuario.getPassword();
+
+        //GUARDAMOS LAS LINEAS DEL FICHERO EN UN ARRAY
+        ArrayList<String> lineas = new ArrayList<String>();
+        try (FileReader fr = new FileReader("RecursosRest/Usuarios")) {
+            BufferedReader br = new BufferedReader(fr);
+
+            //LEEMOS LAS LINEAS DEL FICHERO
+            String linea;
+            while((linea=br.readLine())!=null){
+                lineas.add(linea);
+            }
+            //CERRAMOS EL ARCHIVO
+            br.close();
+            fr.close();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            return false;
+        }
+
+        //ELIMINAMOS DE LA LISTA EL USUARIO A BORRAR
+        lineas.remove(usuarioABorrar);
+
+        //ABRIMOS EL ARCHIVO, Y CREAMOS LO NECESARIO PARA ESCRIBIR
+        try (FileWriter fw = new FileWriter("RecursosRest/Usuarios")) {
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter pw = new PrintWriter(bw);
+
+            //REESCRIBIMOS EL FICHERO, SIN LA LINEA CORRESPONDIENTE AL USUARIO A BORRAR
+            for(String linea:lineas){
+                pw.println(linea);
+            }
+
+            //CERRAMOS EL ARCHIVO
+            pw.close();
+            bw.close();
+            fw.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    //FUNCIÓN PARA ACTUALIZAR UN USUARIO
+    public static boolean actualizarUsuario(ArrayList<Usuario> usuarios){
+
+        String usuarioAActualizar = usuarios.get(0).getNombre() + ":" + usuarios.get(0).getPassword();
+        String usuarioActualizado = usuarios.get(1).getNombre() + ":" + usuarios.get(1).getPassword();
+
+        //GUARDAMOS LAS LINEAS DEL FICHERO EN UN ARRAY
+        ArrayList<String> lineas = new ArrayList<String>();
+        try (FileReader fr = new FileReader("RecursosRest/Usuarios")) {
+            BufferedReader br = new BufferedReader(fr);
+
+            //LEEMOS LAS LINEAS DEL FICHERO
+            String linea;
+            while((linea=br.readLine())!=null){
+                lineas.add(linea);
+            }
+            //CERRAMOS EL ARCHIVO
+            br.close();
+            fr.close();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+            return false;
+        }
+
+        //ACTUALIZAMOS EL USUARIO
+        lineas.set(lineas.indexOf(usuarioAActualizar), usuarioActualizado);
+
+        //ABRIMOS EL ARCHIVO, Y CREAMOS LO NECESARIO PARA ESCRIBIR
+        try (FileWriter fw = new FileWriter("RecursosRest/Usuarios")) {
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter pw = new PrintWriter(bw);
+
+            //REESCRIBIMOS EL FICHERO, CON LA LINEA CORRESPONDIENTE AL USUARIO ACTUALIZADA
+            for(String linea:lineas){
+                pw.println(linea);
+            }
+
+            //CERRAMOS EL ARCHIVO
+            pw.close();
+            bw.close();
+            fw.close();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
     }
 }
--- src/main/java/Main/Controladores/UsuarioController.java
+++ src/main/java/Main/Controladores/UsuarioController.java
@@ -1,8 +1,13 @@
 package Main.Controladores;
-
 import Main.Clases.Usuario;
 import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
 
 
 @RestController
@@ -12,28 +17,142 @@ public class UsuarioController {
 	Gson gson = new Gson();
 
 	@GetMapping
-	//DEVUELVE UN USUARIO
-	public String getUsuario(@RequestBody String JSon) {
+	//USAMOS GET PARA AUTENTICAR AL USUARIO
+	public ResponseEntity<String> autenticarUsuario(@RequestBody String JSon) {
 		try{
 			//INTENTAMOS GENERAR EL USUARIO, A PARTIR DEL CUERPO DEL MENSAJE
 			Usuario usuario = gson.fromJson(JSon, Usuario.class);
 
-			//SI LO GENERAMOS CON EXITO, COMPROBAMOS SI EXISTE
+			//SI ALGUNO DE LOS CAMPOS ES NULL, DEVOLVEMOS QUE EL CUERPO ES INVALIDO
+			if((usuario.getNombre() == null || usuario.getNombre().isEmpty()) || (usuario.getPassword() == null || usuario.getPassword().isEmpty()))
+				return new ResponseEntity<String>("ERROR: CUERPO INVALIDO",HttpStatus.BAD_REQUEST);
+
+			//COMPROBAMOS SI EXISTE EL USUARIO EN LA BASE DE DATOS
 			Usuario usuarioEnBaseDeDatos = Usuario.recuperarUsuario(usuario.getNombre());
 
 			//SI EXISTE COMPROBAMOS LA CONTRASEÑA
-			if(usuarioEnBaseDeDatos != null)
+			if(usuarioEnBaseDeDatos != null){
 				if(usuarioEnBaseDeDatos.getPassword().equals(usuario.getPassword()))
-					return "USUARIO AUTENTICADO";
+					return new ResponseEntity<String>("USUARIO AUTENTICADO", HttpStatus.OK);
 				else
-					return "CONTRASEÑA INVALIDA";
+					return new ResponseEntity<String>("ERROR: CONTRASEÑA INVALIDA", HttpStatus.BAD_REQUEST);
+			}
 			//SI NO EXISTE, MANDAMOS EL MENSAJE DE CREAR NUEVO USUARIO
 			else
-				return "CREAR NUEVO USUARIO";
+				return new ResponseEntity<String>("CREAR NUEVO USUARIO", HttpStatus.OK);
+
 		}
 		catch(Exception e){
-			//e.printStackTrace();
-			return "CUERPO INVALIDO";
+			//SI NO SE PUEDE GENERAR EL USUARIO A PARTIR DEL CUERPO DEL MENSAJE, SE LANZA UN ERROR
+			return new ResponseEntity<String>("ERROR: CUERPO INVALIDO",HttpStatus.BAD_REQUEST);
+		}
+	}
+
+	@PostMapping
+	public ResponseEntity<String> postUsuario(@RequestBody String JSon){
+		try{
+			//INTENTAMOS GENERAR EL USUARIO, A PARTIR DEL CUERPO DEL MENSAJE
+			Usuario usuario = gson.fromJson(JSon, Usuario.class);
+
+			//SI ALGUNO DE LOS CAMPOS ES NULL, DEVOLVEMOS QUE EL CUERPO ES INVALIDO
+			if((usuario.getNombre() == null || usuario.getNombre().isEmpty()) || (usuario.getPassword() == null || usuario.getPassword().isEmpty()))
+				return new ResponseEntity<String>("ERROR: CUERPO INVALIDO",HttpStatus.BAD_REQUEST);
+
+			//SI EL USUARIO YA EXISTE, DEVOLVEMOS QUE ESTE YA EXISTE
+			if(Usuario.recuperarUsuario(usuario.getNombre()) != null)
+				return new ResponseEntity<String>("ERROR: EL USUARIO YA EXISTE",HttpStatus.INTERNAL_SERVER_ERROR);
+
+			//SE INTENTA AÑADIR EL USUARIO A LA BASE DE DATOS, EN CASO DE FALLAR, SE DEVUELVE UN MENSAJE DE ERROR
+			if(Usuario.insertarUsuario(usuario))
+				return new ResponseEntity<String>("USUARIO CREADO Y AUTENTICADO", HttpStatus.CREATED);
+			else
+				return new ResponseEntity<String>("ERROR: NO SE PUDO AÑADIR EL USUARIO",HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+		catch(Exception e){
+			//SI NO SE PUEDE GENERAR EL USUARIO A PARTIR DEL CUERPO DEL MENSAJE, SE LANZA UN ERROR
+			return new ResponseEntity<String>("ERROR: CUERPO INVALIDO",HttpStatus.BAD_REQUEST);
+		}
+	}
+
+	//TODO
+	@PutMapping
+	public ResponseEntity<String> putUsuario(@RequestBody String JSon){
+		try{
+			//DEFINIMOS EL TIPO LISTA DE USUARIOS
+			Type listaDeUsuarios= new TypeToken<ArrayList<Usuario>>() {}.getType();
+
+			//INTENTAMOS GENERAR EL USUARIO y EL USUARIO ACTUALIZADO, A PARTIR DEL CUERPO DEL MENSAJE
+			ArrayList<Usuario> usuarios = gson.fromJson(JSon, listaDeUsuarios);
+
+			//SI ALGUNO DE LOS CAMPOS ES NULL, DEVOLVEMOS QUE EL CUERPO ES INVALIDO
+			if((usuarios.get(0).getNombre() == null || usuarios.get(0).getNombre().isEmpty()) || (usuarios.get(0).getPassword() == null || usuarios.get(0).getPassword().isEmpty()))
+				return new ResponseEntity<String>("ERROR: CUERPO INVALIDO",HttpStatus.BAD_REQUEST);
+			if((usuarios.get(1).getNombre() == null || usuarios.get(1).getNombre().isEmpty()) || (usuarios.get(1).getPassword() == null || usuarios.get(1).getPassword().isEmpty()))
+				return new ResponseEntity<String>("ERROR: CUERPO INVALIDO",HttpStatus.BAD_REQUEST);
+
+			//COMPROBAMOS QUE EL NUEVO NOMBRE NO EXISTA YA (SIEMPRE QUE SEA DISTINTO AL ORIGINAL)
+			if(!usuarios.get(0).getNombre().equals(usuarios.get(1).getNombre()) && Usuario.recuperarUsuario(usuarios.get(1).getNombre()) != null)
+				return new ResponseEntity<String>("ERROR: EL NUEVO NOMBRE DE USUARIO YA ESTA EN USO",HttpStatus.BAD_REQUEST);
+
+			//COMPROBAMOS QUE EL USUARIO EXISTA
+			Usuario usuarioEnBaseDeDatos = Usuario.recuperarUsuario(usuarios.get(0).getNombre());
+			if(usuarioEnBaseDeDatos != null) {
+				//COMPROBAMOS LA CONTRASEÑA
+				if (usuarioEnBaseDeDatos.getPassword().equals(usuarios.get(0).getPassword())) {
+					if(Usuario.actualizarUsuario(usuarios))
+						return new ResponseEntity<String>("USUARIO ACTUALIZADO", HttpStatus.OK);
+					else
+						return new ResponseEntity<String>("ERROR: NO SE HA PODIDO ACTUALIZAR EL USUARIO", HttpStatus.INTERNAL_SERVER_ERROR);
+				}
+				//SI LA CONTRASEÑA ES ERRONEA, SE LANZA UN ERROR
+				else {
+					return new ResponseEntity<String>("ERROR: CONTRASEÑA INVALIDA",HttpStatus.BAD_REQUEST);
+				}
+			}
+			//SI EL USUARIO NO EXISTE, LANZAMOS UN ERROR
+			else
+				return new ResponseEntity<String>("ERROR: EL USUARIO NO EXISTE",HttpStatus.BAD_REQUEST);
+
+		}
+		catch(Exception e){
+			//SI NO SE PUEDE GENERAR EL USUARIO A PARTIR DEL CUERPO DEL MENSAJE, SE LANZA UN ERROR
+			System.out.println("ES ESTE FALLO");
+			return new ResponseEntity<String>("ERROR: CUERPO INVALIDO",HttpStatus.BAD_REQUEST);
+		}
+	}
+
+	@DeleteMapping
+	public ResponseEntity<String> deleteUsuario(@RequestBody String JSon){
+		try{
+			//INTENTAMOS GENERAR EL USUARIO, A PARTIR DEL CUERPO DEL MENSAJE
+			Usuario usuario = gson.fromJson(JSon, Usuario.class);
+
+			//SI ALGUNO DE LOS CAMPOS ES NULL, DEVOLVEMOS QUE EL CUERPO ES INVALIDO
+			if((usuario.getNombre() == null || usuario.getNombre().isEmpty()) || (usuario.getPassword() == null || usuario.getPassword().isEmpty()))
+				return new ResponseEntity<String>("ERROR: CUERPO INVALIDO",HttpStatus.BAD_REQUEST);
+
+			//COMPROBAMOS SI EXISTE EL USUARIO EN LA BASE DE DATOS
+			Usuario usuarioEnBaseDeDatos = Usuario.recuperarUsuario(usuario.getNombre());
+
+			//SI EXISTE COMPROBAMOS LA CONTRASEÑA, EN CASO DE COINCIDIR, ELIMINAMOS EL USUARIO
+			if(usuarioEnBaseDeDatos != null) {
+				if (usuarioEnBaseDeDatos.getPassword().equals(usuario.getPassword())){
+					if(Usuario.eliminarUsuario(usuario))
+						return new ResponseEntity<String>("USUARIO ELIMINADO", HttpStatus.OK);
+					else
+						return new ResponseEntity<String>("ERROR: NO SE HA PODIDO ELIMINAR AL USUARIO", HttpStatus.INTERNAL_SERVER_ERROR);
+				}
+
+				else
+					return new ResponseEntity<String>("ERROR: CONTRASEÑA INVALIDA", HttpStatus.BAD_REQUEST);
+			}
+			//SI NO EXISTE ENVIAMOS UN MENSAJE DICIENDO QUE NO EXISTE
+			else
+				return new ResponseEntity<String>("ERROR: EL USUARIO NO EXISTE",HttpStatus.BAD_REQUEST);
+		}
+		catch(Exception e){
+			//SI NO SE PUEDE GENERAR EL USUARIO A PARTIR DEL CUERPO DEL MENSAJE, SE LANZA UN ERROR
+			return new ResponseEntity<String>("ERROR: CUERPO INVALIDO",HttpStatus.BAD_REQUEST);
 		}
 	}
 
