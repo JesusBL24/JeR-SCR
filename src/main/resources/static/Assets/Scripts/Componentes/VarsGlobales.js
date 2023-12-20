60dab8e94bc0f049176cfbfe194265896b42292f
jagonmes <65248503+jagonmes@users.noreply.github.com>
2023-12-20 15:32:28 +0100

    actualización de las variables globales

--- src/main/resources/static/Assets/Scripts/Componentes/VarsGlobales.js
+++ src/main/resources/static/Assets/Scripts/Componentes/VarsGlobales.js
@@ -1,2 +1,20 @@
 //variables para MENU PRINCIPAL
 var desactivarBoton = false;
+var sesionIniciada = false;
+
+var ip = location.host;
+
+//USUARIO
+class Usuario{
+    constructor() {
+        this.nombre = null;
+        this.password = null;
+    }
+
+    ofuscarContraseña(){
+        if(this.password != '')
+            this.password = md5(this.password);
+    }
+}
+
+var usuario = new Usuario();
